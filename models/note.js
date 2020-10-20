c9d557b513a356914cbad76de02fbe72c0827238
Vilzuge <ville.martas@hotmail.com>
2020-10-20 21:07:31 +0300

    Database toimii?

--- models/note.js
+++ models/note.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const url2 = 'mongodb+srv://databasen_kayttaja:kayttaja123@villen-cluster.pxg9c.mongodb.net/fullstack-db?retryWrites=true&w=majority'
 
 if ( process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -6,7 +7,7 @@ if ( process.env.NODE_ENV !== 'production') {
 
 const url = process.env.MONGODB_URI
 
-mongoose.connect(url)
+mongoose.connect(url2)
 
 const Note = mongoose.model('Note', {
     content: String,
