74b2f533adffb2a402600da670dbb013e12c2c42
Vilzuge <ville.martas@hotmail.com>
2020-10-20 18:05:37 +0300

    Database toimii=

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const app = express()
-
+const Note = require('./models/note')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
 app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('build'))
 
 const logger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -12,11 +15,6 @@ const logger = (request, response, next) => {
     next()
 }
 app.use(logger)
-
-const cors = require('cors')
-app.use(cors())
-
-app.use(express.static('build'))
 
 let notes = [
     {
@@ -43,32 +41,38 @@ app.get('/api', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note
+    .find({})
+    .then(notes => {
+      response.json(notes.map(formatNote))
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-  
-    if ( note ) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+  Note
+    .findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(formatNote(note))
+      } else {
+        response.status(404).end()
+      }
+    }).catch(error => {
+      console.log(error)
+      response.status(404).send({error: 'malformatted id'})
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+  Note
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    }).catch(error => {
+      response.status(400).send({error: 'maformatted id'})
+    })
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0 ? notes.map(n => n.id).sort().reverse()[0] : 1
-    return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -77,23 +81,57 @@ app.post('/api/notes', (request, response) => {
       return response.status(400).json({error: 'content missing'})
     }
   
-    const note = {
+    const note =  new Note ({
       content: body.content,
       important: body.important|| false,
       date: new Date(),
-      id: generateId()
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+    })
+    
+    note
+      .save()
+      .then(formatNote)
+      .then(savedAndFormattedNote => {
+        response.json(savedAndFormattedNote)
+      })
+})
+
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note
+    .findByIdAndUpdate(request.params.id, note, { new: true } )
+    .then(updatedNote => {
+      response.json(formatNote(updatedNote))
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const error = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
 }
-  
 app.use(error)
+
+const formatNote = (note) => {
+  return {
+    content: note.content,
+    date: note.date,
+    important: note.important,
+    id: note._id
+  }
+}
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? notes.map(n => n.id).sort().reverse()[0] : 1
+  return maxId + 1
+}
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
