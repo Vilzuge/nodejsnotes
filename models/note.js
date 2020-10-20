0dce4fede01a84c46091134c705bb4b08a373ff9
Vilzuge <ville.martas@hotmail.com>
2020-10-20 23:06:59 +0300

    toimisko jo

--- models/note.js
+++ models/note.js
@@ -1,17 +1,14 @@
+
 const mongoose = require('mongoose')
 mongoose.set('useUnifiedTopology', true);
-/*
+
 if ( process.env.NODE_ENV !== 'production' ) {
     require('dotenv').config()
 }
 
 const url = process.env.MONGODB_URI
-mongoose.connect(url)
-*/
-
-
-const url = 'mongodb+srv://database_normal_user:normaali123@villen-cluster.pxg9c.mongodb.net/fullstack-user?retryWrites=true&w=majority'
 mongoose.connect(url, { useNewUrlParser: true });
+
 
 const Note = mongoose.model('Note', {
     content: String,
